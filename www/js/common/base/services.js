f859d5e8ecff6f931674dc57535bf32d4ef98ac9
5paceManSpiff <alspore@gmail.com>
2015-09-08 16:35:16 -0400

    Changed api server to testing.breezio portal

--- www/js/common/base/services.js
+++ www/js/common/base/services.js
@@ -1,12 +1,12 @@
 angular.module('neo.base', ['ngResource'])
     .factory('Config', function() {
       var config = {
-          baseUrl: 'https://health.breezio.com',
+          baseUrl: 'https://testing.breezio.com',
           version: '1.0.0',
         };
 
       if (window.cordova)
-          config.baseUrl = 'https://health.breezio.com';
+          config.baseUrl = 'https://testing.breezio.com';
 
       config.apiUrl = config.baseUrl + '/api/1';
 
