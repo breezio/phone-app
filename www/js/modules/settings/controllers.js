975c79f0415e73c12d4f63da65a0a775084ec5fe
5paceManSpiff <alspore@gmail.com>
2015-10-02 13:46:26 -0400

    Moved login button to settings tab

--- www/js/modules/settings/controllers.js
+++ www/js/modules/settings/controllers.js
@@ -1,5 +1,9 @@
 angular.module('neo.settings.controllers', [])
 
-    .controller('SettingsCtrl', function($scope, $rootScope) {
-
+    .controller('SettingsCtrl', function($scope, $rootScope, Auth) {
+      $scope.loggedIn = $rootScope.loggedIn;
+      $scope.currentUser = $rootScope.currentUser;
+      $scope.showUser = $rootScope.showUser;
+      $scope.showLogin = Auth.showLogin;
+      $scope.logout = Auth.logout;
     });
