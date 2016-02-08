ab71c87b9200a839643b44426c61b8a3ebd084be
5paceManSpiff <alspore@gmail.com>
2016-02-08 15:53:10 -0500

    Fetch chats

--- www/js/chats/index.js
+++ www/js/chats/index.js
@@ -11,18 +11,36 @@ angular.module('breezio.chats', [])
   };
 })
 
-.factory('Chat', function($http, $rootScope, ChatToken) {
+.factory('Chats', function($http, $rootScope, ChatToken) {
   var funcs = {};
+
+  funcs.get = function(params) {
+    var params = angular.extend({}, params);
+
+    var promise = $http({
+      method: 'GET',
+      url: $rootScope.config.url + '/conversations',
+      params: params
+    });
+
+    return promise;
+  };
 
   return funcs;
 })
 
-.controller('ChatsCtrl', function($scope, $rootScope, Auth) {
+.controller('ChatsCtrl', function($scope, $rootScope, Auth, Chats) {
   $scope.loaded = false;
 
   $rootScope.$on('chat:token', function() {
     $scope.user = Auth.user();
-    $scope.loaded = true;
+
+    Chats.get().success(function(chats) {
+      $rootScope.$broadcast('chat:chats', chats);
+      console.log(chats);
+      $scope.chats = chats;
+      $scope.loaded = true;
+    });
   });
 
   $rootScope.$on('auth:logged-out', function() {
