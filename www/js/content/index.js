b852881fe5b4733bd8fceefa2dc8475e53d26644
5paceManSpiff <alspore@gmail.com>
2016-03-23 14:52:04 -0400

    Replaced ion-refresher with nav button in content tab

--- www/js/content/index.js
+++ www/js/content/index.js
@@ -5,36 +5,44 @@ angular.module('breezio.content', ['breezio.content.posts', 'breezio.content.use
 })
 
 .controller('ContentCtrl', function($scope, $state, Posts) {
+  $scope.loading = true;
   $scope.start = 0;
   $scope.exhausted = false;
   $scope.posts = [];
 
   $scope.refreshPosts = function() {
     $scope.start = 0;
+    $scope.posts = [];
+    $scope.loading = true;
     Posts.get().then(function(res) {
+      $scope.loading = false;
       $scope.posts = res.data.items;
       $scope.exhausted = false;
-    }).finally(function() {
-      $scope.$broadcast('scroll.refreshComplete');
     });
   };
 
   $scope.loadMore = function() {
-    $scope.start += 20;
-    Posts.get({start: $scope.start}).then(function(res) {
-      $scope.posts = $scope.posts.concat(res.data.items);
+    if (!$scope.loading) {
+      $scope.start += 20;
+      Posts.get({start: $scope.start}).then(function(res) {
+        $scope.posts = $scope.posts.concat(res.data.items);
 
-      if (res.data.items.length < 1) {
-        $scope.exhausted = true;
-      }
-    }).finally(function() {
+        if (res.data.items.length < 1) {
+          $scope.exhausted = true;
+        }
+      }).finally(function() {
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    } else {
       $scope.$broadcast('scroll.infiniteScrollComplete');
-    });
+    }
   };
 
   $scope.openPost = function(post) {
     $state.go('tab.content-post', {postId: post.id});
   };
 
-  $scope.refreshPosts();
+  $scope.$on('$ionicView.loaded', function() {
+    $scope.refreshPosts();
+  });
 });
