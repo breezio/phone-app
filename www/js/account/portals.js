6036a13110b11fcc92cd7a6d456455e04b98bfd6
5paceManSpiff <alspore@gmail.com>
2016-05-06 15:43:19 -0400

    Next button in portal view displays spinner on click

--- www/js/account/portals.js
+++ www/js/account/portals.js
@@ -1,19 +1,26 @@
 angular.module('breezio.account.portals', [])
 
-.controller('PortalCtrl', function($scope, $http, $rootScope, $ionicLoading, $state, Config) {
-  $scope.form = {portal: Config.portal};
+.controller('PortalCtrl', function($scope, $http, $rootScope, $ionicLoading, $ionicNativeTransitions, Config) {
+  $scope.form = {portal: Config.portal, spin: false};
 
   $scope.connect = function() {
     var url = Config.toHost($scope.form.portal);
+
+    $scope.form.spin = true;
 
     $http({
       method: 'GET',
       url: url + '/api/1/posts?limit=1'
     }).then(function(ret) {
+      $scope.form.spin = false;
       if (ret.status == 200) {
         Config.setPortal($scope.form.portal, url);
         $rootScope.reload = true;
-        $state.go('tab.content', {}, {reload: true});
+
+        $ionicNativeTransitions.stateGo('tab.content', {}, {
+          type: 'fade',
+          duration: 300
+        }, { reload: true });
       } else {
         $ionicLoading.show({
           template: 'Could not reach ' + url,
